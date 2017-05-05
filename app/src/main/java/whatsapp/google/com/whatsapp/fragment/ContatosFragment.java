706bf5ea22998140c7b0a54e82d5f2df73e49a5c
MarcMelo <marcilio_sergio@hotmail.com>
2017-05-05 13:19:33 -0300

    Listando usuários

--- app/src/main/java/whatsapp/google/com/whatsapp/fragment/ContatosFragment.java
+++ app/src/main/java/whatsapp/google/com/whatsapp/fragment/ContatosFragment.java
@@ -6,14 +6,31 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 import whatsapp.google.com.whatsapp.R;
+import whatsapp.google.com.whatsapp.config.FirebaseConnection;
+import whatsapp.google.com.whatsapp.model.Contato;
+import whatsapp.google.com.whatsapp.util.Preferencias;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ContatosFragment extends Fragment {
 
+
+    private ListView listView;
+    private ArrayAdapter adapter;
+    private ArrayList<String> contatos;
+    private DatabaseReference databaseReference;
 
     public ContatosFragment() {
         // Required empty public constructor
@@ -23,8 +40,46 @@ public class ContatosFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        contatos = new ArrayList<>();
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contatos, container, false);
+        View view = inflater.inflate(R.layout.fragment_contatos, container, false);
+
+        listView = (ListView)view.findViewById(R.id.lv_contatos);
+
+        adapter = new ArrayAdapter(
+          getActivity(), R.layout.lista_contato, contatos
+        );
+
+        listView.setAdapter(adapter);
+
+        Preferencias preferencias = new Preferencias(getActivity());
+        String identificadorUsuarioLogado = preferencias.getIdentificador();
+        databaseReference = FirebaseConnection.getFirebaseReference().child("contatos").child(identificadorUsuarioLogado);
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                contatos.clear();
+
+                for(DataSnapshot dados: dataSnapshot.getChildren()){
+                    Contato contato = dados.getValue(Contato.class);
+                    contatos.add(contato.getNomeUsuario());
+                }
+
+                adapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+        return view;
     }
 
 }
